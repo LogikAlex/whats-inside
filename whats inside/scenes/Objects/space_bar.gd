d4a801b9846547ac43d2851fe58c65b1b4227fc9
LogikAlex <136632902+LogikAlex@users.noreply.github.com>
2026-01-01 23:24:18 +0100

    doing the jump animation
    - i'm falling apart
    - can't wait to continue working !!!

--- whats inside/scenes/Objects/space_bar.gd
+++ whats inside/scenes/Objects/space_bar.gd
@@ -1,12 +1,15 @@
 extends StaticBody2D
 
+@export var interactable = true
+
 @onready var space_sprite = $Sprite2D
 @onready var collisions = $CollisionPolygon2D
 
 func _process(_delta: float) -> void:
-	if Input.is_action_pressed("interact"):
-		space_sprite.frame = 1
-		collisions.disabled = true
-	else:
-		space_sprite.frame = 0
-		collisions.disabled = false
+	if interactable:
+		if Input.is_action_pressed("interact"):
+			space_sprite.frame = 1
+			collisions.disabled = true
+		else:
+			space_sprite.frame = 0
+			collisions.disabled = false
